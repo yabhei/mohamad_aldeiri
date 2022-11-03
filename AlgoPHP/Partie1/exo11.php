c7667b210a1e26373f2f63c21ce20f13fbef3252
AL DEIRI Mohamad <mhdderi93@gmail.com>
2022-11-03 12:01:49 +0100

    Ajout l'exo11

---
+++ AlgoPHP/Partie1/exo11.php
@@ -0,0 +1,17 @@
+<h1>Exercice 10</h1>
+
+<p> 
+A partir d’un montant à payer et d’une somme versée pour régler un achat, 
+écrire l’algorithme qui affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 5 €, de pièces de 2 € et 1 €.
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$x = [];
+array_push($x,10);
+// print_r($x);
+
+
+?>
