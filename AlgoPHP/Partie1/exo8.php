acd7eca1e7e23c261257c4436063d7cc55a99cd5
AL DEIRI Mohamad <mhdderi93@gmail.com>
2022-11-03 09:59:05 +0100

    Modifié l'exo8 et ajout l'exo9

--- AlgoPHP/Partie1/exo8.php
+++ AlgoPHP/Partie1/exo8.php
@@ -7,12 +7,19 @@ Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé
 <h2>Résultat</h2>
 
 <?php
-$num = 8;
-$resultat = 0;
+$num = 8; // user number
+$resultat = 0; // for calculate the result
+$i = 1; // counter for while loop 
 echo "Table de ".$num.  " : <br> " ;
-for($i = 1; $i <= 10; $i++){
+// for($i = 1; $i <= 10; $i++){
+//     $resultat = $i * $num;
+//     echo $i. "x" .$num. " = ".$resultat. "<br>";
+// }
+
+while($i<=10){
     $resultat = $i * $num;
     echo $i. "x" .$num. " = ".$resultat. "<br>";
+    $i++;
 }
 
 
---
+++ AlgoPHP/Partie1/exo9.php
@@ -0,0 +1,17 @@
+<h1>Exercice 9</h1>
+
+<p> 
+Nous souhaitons savoir si une personne est imposable en fonction de son âge et de son sexe.
+Si la personne est une femme dont l’âge est compris entre 18 et 35 ans ou si c’est un homme de plus de 20 ans, 
+alors celle-ci est imposable (sinon ce n’est pas le cas, « non imposable »).
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+$age =0;
+$sexe = "H";
+if($sexe == "H")
+
+
+?>
