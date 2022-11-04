3095161ae1899ba4650aa596b96901daa0f93fec
AL DEIRI Mohamad <mhdderi93@gmail.com>
2022-11-04 15:32:09 +0100

    Modifier exo10

--- AlgoPHP/Partie1/exo10.php
+++ AlgoPHP/Partie1/exo10.php
@@ -8,34 +8,39 @@ A partir d’un montant à payer et d’une somme versée pour régler un achat,
 <h2>Résultat</h2>
 
 <?php
-$pmont = 152; //Montant à payer
-$vmont = 200; //Montant versé
+$pmont = 157; //Montant à payer
+$vmont = 500; //Montant versé
 $rest =$vmont - $pmont;   //Reste à payer
 
-echo "Montant à payer : ".$pmont. " £ <br> Montant versé : ".$vmont. " £ <br> Reste à payer : ".$rest. " £";
-$i = $reset; // counter of while loop
-$rendue =[];
+echo "Montant à payer : ".$pmont. " £ <br> Montant versé : ".$vmont. " £ <br> Reste à payer : ".$rest. " £ <br>";
+$i = $rest; // counter of while loop
+$c =0;
+$j=0;
+$o=0;
+$l=0;
 // array_push($rendue,10);
 // echo $rendue;
 
 
 for($i = $rest; $i >0 ; $i){
     if($rest >=10){
-                $reset = $reset - 10;
-                array_push($rendue,10);
+                $rest = $rest - 10;
+                $c=$c+1;
     }elseif($rest >=5){
-                $reset = $reset - 5;
-                array_push($rendue,5);
+                $rest = $rest - 5;
+                $j=$j+1;
     }elseif($rest >=2){
-                $reset = $reset - 2;
-                array_push($rendue,2);
+                $rest = $rest - 2;
+                $o=$o+1;
     }elseif($rest >=1){
-                $reset = $reset - 1;
-                array_push($rendue,1);
+                $rest = $rest - 1;
+                $l=$l+1;
     }
             $i = $rest;
 
 }
+
+echo  $c." billets de 10 € <br>".$j.  " billet de 5 € <br>" .$o. " pièce de 2 € <br>" .$l. " pièce de 1 €"
 // while($i !== 0){
 //     if($rest >=10){
 //         $reset = $reset - 10;
