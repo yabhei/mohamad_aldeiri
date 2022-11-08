aba3ed56c268dd280e7246485cdc5f8d480c1dda
AL DEIRI Mohamad <mhdderi93@gmail.com>
2022-11-08 10:28:56 +0100

    modifier exo14

--- AlgoPHP/Partie1/exo14.php
+++ AlgoPHP/Partie1/exo14.php
@@ -10,29 +10,6 @@ Calculer l'âge exact d'une personne à partir de sa date de naissance (en anné
 <h2> Résultat</h2>
 
 
-<!-- <?php
-
-$day=11;
-$month=02;
-$year=1993;
-echo "Your date of  birth is : ". sprintf("%02s", $day)."/". sprintf("%02s", $month)."/".$year."<br>";
-$dob=date_create($day."/".$month."/".$year);
-$today=date_create(date('d/m/Y'));
-$diff=date_diff($dob,$today);
-
-echo "Your age is : <br>".date("Y") - $year." ans ";
-echo date("m") - $month." Mois ";
-echo date("d") - $day." jours <br>";
-?> -->
-
-<!-- <?php
-$bday = new DateTime('01/02/1993'); // Your date of birth
-$today = new Datetime(date('d/m/y'));
-$diff = $today->diff($bday);
-printf(' Your age : %d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
-printf("\n");
-?> -->
-
 <?php 
 
 //date of birth
