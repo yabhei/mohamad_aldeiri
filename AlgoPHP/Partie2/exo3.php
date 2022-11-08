80a1a2fa39a008e7c95317868af854a984bbc93d
AL DEIRI Mohamad <mhdderi93@gmail.com>
2022-11-08 14:45:08 +0100

    Ajout exo4

---
+++ AlgoPHP/Partie2/exo3.php
@@ -0,0 +1,15 @@
+<h1>Exercice 3 </h1>
+
+<p> 
+Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir dans un nouvel onglet (_blank).
+
+
+
+</p>
+
+<h2> Résultat</h2>
+
+
+<a href="https://elan-formation.eu/accueil" target="_blank">Elan Formation</a>
+
+
